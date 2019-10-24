32ae7782309e6d40fb4ac7168a50a8f8dac566d7
AlejandroTrejos <51036952+AlejandroTrejos@users.noreply.github.com>
2019-10-24 12:46:13 -0500

    10/24

--- client/src/components/flow.js
+++ client/src/components/flow.js
@@ -10,7 +10,7 @@ function Flow() {
             <div>
                 <ul>
                     <h3>Start of digram modules</h3>
-                    <li>a</li>
+                    <li>these will be sketched using p5 and then implemented with a React/p5 wrapper</li>
                     <li>a</li>
                     <li>a</li>
                     <li>a</li>
