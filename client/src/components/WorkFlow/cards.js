7110e03e911033453cf244c0abaa16f930fdca22
atrejos <alextre.edu@gmail.com>
2019-10-29 13:10:27 -0500

    10/29

--- client/src/components/WorkFlow/cards.js
+++ client/src/components/WorkFlow/cards.js
@@ -1,74 +1,34 @@
 import React from 'react';
-import { Row, Col, Container, Card, CardImg, CardText, CardBody,
-    CardTitle, CardSubtitle, Button } from 'reactstrap';
-import styled from 'styled-components';
+import { Card, CardBody, Button } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 //Flow Diagram Page
 
-// class Hoverable extends React.Component {
-//     state = { hovered: false };
-//     render() {
-//         return (
-//             <div
-//                 onMouseEnter={() => this.setState({ hovered: true })}
-//                 onMouseLeave={() => this.setState({ hovered: false })}
-//             >
-//                 {this.props.children(this.state.hovered)}
-//             </div>
-//         );
-//     }
-// }
+class Cardy extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isComplete: false
+        };
+    }
+    render() {
+        const stepName = this.props.stepName;
+        const stepDesc = this.props.stepDesc;
+        return(
+                <div>
+                    <Card style={{ width: 'auto', backgroundColor: 'Azure' }}>
+                        <CardBody>
+                            <h3>{stepName}</h3>
+                            <p>{stepDesc}</p>
+                            <div>
+                                <Button>Button</Button>
+                            </div>
+                        </CardBody>
+                    </Card>
+                </div>
+        );
 
-// function Cardy() {
-//     return(
-//         <Hoverable>
-//                 {hovered => <div>{hovered ? <div><Card style={{
-//                 height: 50,
-//                 width: 100,
-//                 backgroundColor: 'Azure'
-//             }}>
-//                 <CardBody>
-//                     <CardTitle>{}</CardTitle>
-//                     <CardText>{}</CardText>
-//                 </CardBody>
-//             </Card></div> : <div><Card style={{
-//                 height: 50,
-//                 width: 100,
-//                 backgroundColor: 'blue'
-//             }}>
-//                 <CardBody>
-//                     <CardTitle>{}</CardTitle>
-//                     <CardText>{}</CardText>
-//                 </CardBody>
-//             </Card>}
-//             </div>
-//         </Hoverable>
-//     <div>
-//         <Card style={{
-//             height: 50,
-//             width: 100,
-//             backgroundColor: 'Azure'
-//         }}>
-//             <CardBody>
-//                 <CardTitle>{}</CardTitle>
-//                 <CardText>{}</CardText>
-//             </CardBody>
-//         </Card>
-//     </div>
-
-function Cardy() {
-    return(
-            <div>
-                <Card style={{ width: 'auto', backgroundColor: 'Azure' }}>
-                    <CardBody>
-                        <CardTitle>Title</CardTitle>
-                        <CardText>text</CardText>
-                        <Button>Button</Button>
-                    </CardBody>
-                </Card>
-            </div>
-    );
+    }
 }
 
 export default Cardy;
--- client/src/components/WorkFlow/flow.js
+++ client/src/components/WorkFlow/flow.js
@@ -6,115 +6,58 @@ import Cardy from './cards';
 
 //Flow Diagram Page
 
-function Flow() {
-    return (
-        <div>
-            <h1>Flow Diagram Page</h1>
-            <br></br>
-            <div>
+class Flow extends React.Component {
+         render() {
+            return (
                 <div>
-                    <Container>
-                        <Col>
-                            <Row>
-                                <Col></Col>
-                                <Col></Col>
-                                <Col >
-                                    <Cardy id="card-1"/>
-                                </Col>
-                                <Col></Col>
-                                <Col></Col>
-                            </Row>
-                            <br></br>
-                            <Row>
-                                <Col></Col>
+                    <h1>Flow Diagram Page</h1>
+                    <br></br>
+                    <div>
+                        <div>
+                            <Container>
                                 <Col>
-                                    <Cardy id="card-2"/>
+                                    <Row>
+                                        <Col></Col><Col></Col><Col ><Cardy stepName="step 1" stepDesc="description of step 1"/></Col><Col></Col><Col></Col>
+                                    </Row>
+                                    <br></br>
+                                    <Row>
+                                        <Col></Col><Col><Cardy stepName="step 2" stepDesc="description of step 2"/></Col><Col><Cardy stepName="step 3" stepDesc="description of step 3"/></Col><Col><Cardy stepName="step 4" stepDesc="description of step 4"/></Col><Col></Col>
+                                    </Row>
+                                    <br></br>
+                                    <Row>
+                                        <Col></Col><Col><Cardy stepName="step 5" stepDesc="description of step 5"/></Col><Col></Col><Col><Cardy stepName="step 6" stepDesc="description of step 6"/></Col><Col></Col>
+                                    </Row>
+                                    <br></br>
+                                    <Row>
+                                        <Col></Col><Col></Col><Col></Col><Col><Cardy stepName="step 7" stepDesc="description of step 7"/></Col><Col></Col>
+                                    </Row>
+                                    <br></br>
+                                    <Row>
+                                        <Col></Col><Col></Col><Col><Cardy stepName="step 8" stepDesc="description of step 8"/></Col><Col></Col><Col></Col>
+                                    </Row>
+                                    <br></br>
+                                    <Row>
+                                        <Col></Col><Col></Col><Col><Cardy stepName="step 9" stepDesc="description of step 9"/></Col><Col></Col><Col></Col>
+                                    </Row>
+                                    <br></br>
+                                    <Row>
+                                        <Col></Col><Col></Col><Col><Cardy stepName="step 10" stepDesc="description of step 10"/></Col><Col></Col><Col></Col>
+                                    </Row>
+                                    <br></br>
+                                    <Row>
+                                        <Col></Col><Col></Col><Col><Cardy stepName="step 11" stepDesc="description of step 11"/></Col><Col></Col><Col></Col>
+                                    </Row>
+                                    <br></br>
+                                    <Row>
+                                        <Col></Col><Col></Col><Col><Cardy stepName="step 12" stepDesc="description of step 12"/></Col><Col></Col><Col></Col>
+                                    </Row>
                                 </Col>
-                                <Col>
-                                    <Cardy id="card-3"/>
-                                </Col>
-                                <Col>
-                                    <Cardy id="card-4"/>
-                                </Col>
-                                <Col></Col>
-                            </Row>
-                            <br></br>
-                            <Row>
-                                <Col></Col>
-                                <Col>
-                                <Cardy id="card-5"/>
-                                </Col>
-                                <Col></Col>
-                                <Col>
-                                <Cardy id="card-6"/>
-                                </Col>
-                                <Col></Col>
-                            </Row>
-                            <br></br>
-                            <Row>
-                                <Col></Col>
-                                <Col></Col>
-                                <Col></Col>
-                                <Col>
-                                <Cardy id="card-7"/>
-                                </Col>
-                                <Col></Col>
-                            </Row>
-                            <br></br>
-                            <Row>
-                                <Col></Col>
-                                <Col></Col>
-                                <Col>
-                                <Cardy id="card-8"/>
-                                </Col>
-                                <Col></Col>
-                                <Col></Col>
-                            </Row>
-                            <br></br>
-                            <Row>
-                                <Col></Col>
-                                <Col></Col>
-                                <Col>
-                                <Cardy id="card-9"/>
-                                </Col>
-                                <Col></Col>
-                                <Col></Col>
-                            </Row>
-                            <br></br>
-                            <Row>
-                                <Col></Col>
-                                <Col></Col>
-                                <Col>
-                                <Cardy id="card-10"/>
-                                </Col>
-                                <Col></Col>
-                                <Col></Col>
-                            </Row>
-                            <br></br>
-                            <Row>
-                                <Col></Col>
-                                <Col></Col>
-                                <Col>
-                                <Cardy id="card-11"/>
-                                </Col>
-                                <Col></Col>
-                                <Col></Col>
-                            </Row>
-                            <br></br>
-                            <Row>
-                                <Col></Col>
-                                <Col></Col>
-                                <Col>
-                                <Cardy id="card-12"/>
-                                </Col>
-                                <Col></Col>
-                                <Col></Col>
-                            </Row>
-                        </Col>
-                    </Container>
+                            </Container>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    )
+            )
+    }
 }
+
 export default Flow;
